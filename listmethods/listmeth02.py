a6dfb2e87f0605b584a3eb924e33d64a34adb7bb
GGlitter <gabriela.garcia20j@gmail.com>
2023-07-09 23:12:58 +0000

    manipulating lists

---
+++ listmethods/listmeth02.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+colors = [ 'blue', 'purple', 'green', 'yellow', 'red']
+print(colors)
+# this will print out the list of colors
+colors.append('pink')
+#this will add pink to the end of the list
+colors.extend(['ivory'])
+#by extending, this part will add what is in [] to the list
+print(colors)
+
+
